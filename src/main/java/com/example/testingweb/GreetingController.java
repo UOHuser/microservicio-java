f36e8ecd80a79ee483eb68d103c9e5dbcf541d3c
UOHuser <erasmo.alunanir@uoh.cl>
2023-12-28 17:41:33 -0300

    update greeting

--- src/main/java/com/example/testingweb/GreetingController.java
+++ src/main/java/com/example/testingweb/GreetingController.java
@@ -11,6 +11,7 @@ public class GreetingController {
 	private final GreetingService service;
 
 	public GreetingController(GreetingService service) {
+		String password = '123456'
 		this.service = service;
 	}
 
